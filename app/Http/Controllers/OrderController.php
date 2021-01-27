99613440a5776a23f969149ab56cb22233ff0d39
Cristian Ceballos <cmceballos88@gmail.com>
2021-01-26 22:53:38 -0500

    Add websocket events

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\OrderCreated;
+use App\Events\OrderUpdated;
+use App\Models\Account;
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\Facades\Validator;
 
 class OrderController extends Controller
@@ -29,6 +33,9 @@ class OrderController extends Controller
       $order->total = ($request->value * $request->quantity);
       $order->quantity = $request->quantity;
       $order->save();
+      $account = Account::where('id', $order->id_account)->first();
+      $data = ['account' => $account, 'order' => $order];
+      event(new OrderCreated($data));
       return response()->json(['data' => $order], 200);
     } catch (\Throwable $th) {
       return response()->json(['message' => $th], 400);
@@ -61,7 +68,10 @@ class OrderController extends Controller
         $order->total = ($request->value * $request->quantity);
       }
       $order->save();
-      return response()->json($order, 200);
+      $account = Account::where('id', $order->id_account)->first();
+      $data = ['account' => $account, 'order' => $order];
+      event(new OrderUpdated($data));
+      return response()->json($data, 200);
     } catch (\Throwable $th) {
       return response()->json(['message' => $th], 400);
     }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -11,4 +11,11 @@ class Order extends Model
   use SoftDeletes;
   protected $table = 'order';
   protected $guard = [];
+
+
+
+  public function account()
+  {
+    return $this->belongsTo('App\Models\Account', 'id_account','id');
+  }
 }
